42ba3071e325b84e7abd9e9de1d33833fb624c83
kirus <kirushanth.2@gmail.com>
2021-03-06 22:48:14 +0000

    harden express

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const cookieSession = require('cookie-session');
+const helmet = require('helmet');
+var csrf = require('csurf');
 const os = require('os');
 
 const app = express();
 
+app.use(cookieSession({
+    name: 'session',
+    keys: ['key1', 'key2']
+}))
+
+app.use(helmet());
+app.use(csrf({ cookie: true });
+
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
 
-app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+app.listen(process.env.PORT || 8080, () =>
+  console.log(`Listening on port ${process.env.PORT || 8080}!`);
+);
