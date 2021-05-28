f5224ba3cb870f8fa9eff22c68d0969419777615
kirus <kirushanth.2@gmail.com>
2021-05-28 19:35:59 +0000

    network swap

--- src/client/js/components/partials/swap/SwapNetworkToggle.jsx
+++ src/client/js/components/partials/swap/SwapNetworkToggle.jsx
@@ -6,22 +6,80 @@ import Wallet from '../../../utils/wallet';
 import Metrics from '../../../utils/metrics';
 import EventManager from '../../../utils/events';
 
+import TokenIconImg from './../TokenIconImg';
+
 export default class SwapNetworkToggle extends Component {
   constructor(props) {
     super(props);
+
+    this.handleDropdownClick = this.handleDropdownClick.bind(this);
+
+    this.NETWORKS = _.map([
+      "ETH",
+      "0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0", // MATIC
+      "0xB8c77482e45F1F44dE1745F52C74426C631bDD52" // BNB
+    ], function(v) { return Wallet.findTokenById(v) });
+  }
+
+  handleDropdownClick(token) {
+    return function handleClick(e) {
+    }.bind(this);
   }
 
   render() {
+    var networkList = _.map(this.NETWORKS, function(v) {
+      return (
+        <a href="#"
+          onClick={this.handleDropdownClick(v)}
+          className={classnames("dropdown-item level column is-mobile")}>
+          <span className="level-left my-2">
+            <span className="level-item">
+              <TokenIconImg
+                size={35}
+                token={v} />
+            </span>
+            <span className="level-item has-text-grey">{v.symbol}</span>
+          </span>
+        </a>
+      );
+    }.bind(this));
+
     return (
-      <div className="alpha-banner notification is-warning is-light">
-        <div className="content">
-          <p className="is-size-6">
-            <b>Welcome to the Polkaswitch Alpha launch!</b>
-          </p>
-          <p>As we work closely with our technology partners for the Mainnet launch, you may experience intermittent issues.</p>
-          <p>To be able to perform swaps on the Polkaswitch Alpha, we have built the following tool to conveniently mint test tokens into your connected wallet.</p>
-          <p className="is-italic">100 Tokens will be added under METH, MUNI, MSUSHI and MBAL</p>
+      <div className="swap-network-toggle notification">
+        <div className="level is-mobile option">
+          <div className="level-left">
+            <div className="level-item">
+              <span>
+                <b>Choose Network</b>
+                <span
+                  className="hint-icon hint--bottom hint--medium"
+                  aria-label="You can expedite your transaction by paying more Gas Fees. You can choose between either faster transactions or cheaper fees."
+                >?</span>
+              </span>
+            </div>
+          </div>
+
+          <div className="level-right">
+            <div className="level-item">
+              <div className="dropdown is-active">
+                <div className="dropdown-trigger">
+                  <button className="button" aria-haspopup="true" aria-controls="dropdown-menu">
+                    <span>Dropdown button</span>
+                    <span className="icon is-small">
+                      <i className="fas fa-angle-down" aria-hidden="true"></i>
+                    </span>
+                  </button>
+                </div>
+                <div className="dropdown-menu" id="dropdown-menu" role="menu">
+                  <div className="dropdown-content">
+                    {networkList}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+
       </div>
     );
   }
