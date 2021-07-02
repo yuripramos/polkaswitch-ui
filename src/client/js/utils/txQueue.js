405731c9849c719b2b8d62ce64c8c0629af72cf8
ск <sk@iMac-sk.local>
2021-07-02 09:13:39 -0700

    fix issue (Transaction History is not preserved on Browser Refresh #9)

--- src/client/js/utils/txQueue.js
+++ src/client/js/utils/txQueue.js
@@ -1,15 +1,21 @@
 import _ from "underscore";
 import EventManager from './events';
 import * as ethers from 'ethers';
-
+import Wallet from "./wallet";
+let store = require('store')
 const BigNumber = ethers.BigNumber;
 const Utils = ethers.utils;
 const Contract = ethers.Contract;
 
 export default {
+  _signerAddress: '',
   _queue: {},
 
   queuePendingTx: function(data, confirms) {
+    const queue = this.getQueue()
+    if (queue) {
+      this._queue = queue;
+    }
     var hash = data.tx.hash;
     data.lastUpdated = Date.now();
     data.completed = false;
@@ -17,7 +23,7 @@ export default {
 
     // TODO serialize _queue into window.localStorage eventually, so not lost on refresh
     this._queue[hash] = data;
-
+    store.set(this._signerAddress, this._queue)
     EventManager.emitEvent('txQueueUpdated', hash);
 
     data.tx.wait(confirms || 1).then(function(txReceipt) {
@@ -28,6 +34,7 @@ export default {
       this._queue[hash].success = true;
       this._queue[hash].completed = true;
       this._queue[hash].lastUpdated = Date.now();
+      store.set(this._signerAddress, this._queue)
       EventManager.emitEvent('txQueueUpdated', hash);
       EventManager.emitEvent('txSuccess', hash);
     }.bind(this)).catch(function(err) {
@@ -35,21 +42,24 @@ export default {
       this._queue[hash].completed = true;
       this._queue[hash].success = false;
       this._queue[hash].lastUpdated = Date.now();
+      store.set(this._signerAddress, this._queue)
       EventManager.emitEvent('txQueueUpdated', hash);
       EventManager.emitEvent('txFailed', hash);
     }.bind(this));
   },
 
   getQueue: function() {
-    return this._queue;
+    this._signerAddress = Wallet.currentAddress()
+    const queue = store.get(this._signerAddress) ? store.get(this._signerAddress) : {};
+    return queue;
   },
 
   numOfPending: function() {
-    return _.keys(this._queue).length;
+    return _.keys(this.getQueue()).length;
   },
 
   getTx: function(nonce) {
-    return this._queue[nonce];
+    return this.getQueue()[nonce];
   }
 };
 
