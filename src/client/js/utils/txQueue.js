5f1e54e389f548c07a586ae9ce77e26443b278f8
davidkoller318 <davidkoller.info@gmail.com>
2021-07-06 20:32:57 -0700

    fix issue by comment.

--- src/client/js/utils/txQueue.js
+++ src/client/js/utils/txQueue.js
@@ -23,34 +23,36 @@ export default {
 
     // TODO serialize _queue into window.localStorage eventually, so not lost on refresh
     this._queue[hash] = data;
-    store.set(this._signerAddress, this._queue)
-    EventManager.emitEvent('txQueueUpdated', hash);
+    if (this._signerAddress && (this._signerAddress.length > 0)) {
+      store.set(this._signerAddress, this._queue)
+      EventManager.emitEvent('txQueueUpdated', hash);
 
-    data.tx.wait(confirms || 1).then(function(txReceipt) {
-      console.log(txReceipt);
-      console.log(`Transaction Hash: ${txReceipt.transactionHash}`);
-      console.log(`Gas Used: ${txReceipt.gasUsed.toString()}`);
-      this._queue[hash].receipt = txReceipt;
-      this._queue[hash].success = true;
-      this._queue[hash].completed = true;
-      this._queue[hash].lastUpdated = Date.now();
-      store.set(this._signerAddress, this._queue)
-      EventManager.emitEvent('txQueueUpdated', hash);
-      EventManager.emitEvent('txSuccess', hash);
-    }.bind(this)).catch(function(err) {
-      console.error(err);
-      this._queue[hash].completed = true;
-      this._queue[hash].success = false;
-      this._queue[hash].lastUpdated = Date.now();
-      store.set(this._signerAddress, this._queue)
-      EventManager.emitEvent('txQueueUpdated', hash);
-      EventManager.emitEvent('txFailed', hash);
-    }.bind(this));
+      data.tx.wait(confirms || 1).then(function (txReceipt) {
+        console.log(txReceipt);
+        console.log(`Transaction Hash: ${txReceipt.transactionHash}`);
+        console.log(`Gas Used: ${txReceipt.gasUsed.toString()}`);
+        this._queue[hash].receipt = txReceipt;
+        this._queue[hash].success = true;
+        this._queue[hash].completed = true;
+        this._queue[hash].lastUpdated = Date.now();
+        store.set(this._signerAddress, this._queue)
+        EventManager.emitEvent('txQueueUpdated', hash);
+        EventManager.emitEvent('txSuccess', hash);
+      }.bind(this)).catch(function (err) {
+        console.error(err);
+        this._queue[hash].completed = true;
+        this._queue[hash].success = false;
+        this._queue[hash].lastUpdated = Date.now();
+        store.set(this._signerAddress, this._queue)
+        EventManager.emitEvent('txQueueUpdated', hash);
+        EventManager.emitEvent('txFailed', hash);
+      }.bind(this));
+    }
   },
 
   getQueue: function() {
-    this._signerAddress = Wallet.currentAddress()
-    const queue = store.get(this._signerAddress) ? store.get(this._signerAddress) : {};
+    this._signerAddress = Wallet.currentAddress();
+    const queue = store.get(this._signerAddress);
     return queue;
   },
 
