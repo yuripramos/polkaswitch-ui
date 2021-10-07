7cc345261879335c7ae1bd20409d878910593826
davidsoft <davidkoller.info@gmail.com>
2021-10-06 18:11:35 -0700

    update tx history

--- src/client/js/components/partials/swap/TxnHistory.jsx
+++ src/client/js/components/partials/swap/TxnHistory.jsx
@@ -4,29 +4,67 @@ import TxStatusView from "../TxStatusView";
 import TxQueue from '../../../utils/txQueue';
 
 export default function TxnHistory() {
+  const itemMax = 3;
   const [queue, setQueue] = useState({});
+  const [page, setPage] = useState(1);
+  const [maxPage, setMaxPage] = useState(1);
+
+  useEffect(() =>{
+    setMaxPage(1);
+    setPage(1);
+    const length = Object.keys(queue).length
+    if (length > 0) {
+      let extraPages = 1
+      if (length % itemMax === 0) {
+        extraPages = 0
+      }
+      setMaxPage(Math.floor(length / itemMax) + extraPages)
+    }
+  }, [queue, itemMax]);
+
   // Fetch Data
   useEffect(() =>{
+    setPage(1)
     setQueue(TxQueue.getQueue())
   }, []);
 
+  const filteredList = useMemo(() => {
+    return (
+        queue && Object.keys(queue).map((key) => queue[key]).slice(itemMax * (page - 1), page * itemMax)
+    )
+  }, [queue, itemMax, page])
+
   return (
-    <div className="grid-table">
+      <div>
       {
-        Object.keys(queue).length > 0 &&
-            < div className="title-bar">Trade History</div>
-      }
-      <div className="body">
-        {_.map(queue, function(item, i) {
-          return (
-              <div
-                key={i}
-                className="row">
-                <TxStatusView key={i} data={item} />
+        queue && Object.keys(queue).length > 0 &&
+          <div className="grid-table">
+            <div className="title-bar">Trade History</div>
+            <div className="body">
+              {
+                filteredList &&
+                _.map(filteredList, function (item, i) {
+                  return (
+                      <div
+                          key={i}
+                          className="row">
+                        <TxStatusView key={i} data={item}/>
+                      </div>
+                  );
+                })
+              }
+            </div>
+            <div className="page-button">
+              <div onClick={() => setPage(page === 1 ? page : page - 1)}>
+                <div className="arrow">←</div>
               </div>
-          );
-        })}
+              <div>{'Page ' + page + ' of ' + maxPage}</div>
+              <div onClick={() => setPage(page === maxPage ? page : page + 1)}>
+                <div className="arrow">→</div>
+              </div>
+            </div>
+          </div>
+      }
       </div>
-    </div>
   )
 }
