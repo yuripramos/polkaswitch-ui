7a23ae0ecc4c9682aede3b599a66c836ac0a0cbb
tiger-pko <kiru@polkaswitch.com>
2021-10-22 17:01:54 +0000

    Revert bad conflict merge

--- src/client/js/components/partials/swap/SwapTokenSearchSlide.jsx
+++ src/client/js/components/partials/swap/SwapTokenSearchSlide.jsx
@@ -17,12 +17,23 @@ export default class SwapTokenSearchSlide extends Component {
   }
 
   render() {
+    var isCrossChainEnabled = TokenListManager.isCrossChainEnabled();
+    var network = this.props.network || TokenListManager.getCurrentNetworkConfig();
+    var crossChainTokens = _.map(network.supportedCrossChainTokens, function(v) {
+      return TokenListManager.findTokenById(v, network)
+    });
+
+    // passing undefined, will default to the original network list.
+    // we only want to show the cross-chain tokens from the sending chain
+    var tokenList = isCrossChainEnabled && this.props.isFrom ? crossChainTokens : undefined;
+
     return (
       <div className="page page-stack page-view-search">
         <div className="page-inner">
           <TokenSearchBar
             inline={true}
             network={this.props.network}
+            tokenList={tokenList}
             focused={this.props.showSearch}
             placeholder={"Try DAI, USDT or Ethereum ... "}
             handleClose={this.props.handleSearchToggle("to")} // "to" is arbitary
