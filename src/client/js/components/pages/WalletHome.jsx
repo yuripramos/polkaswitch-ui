a9c980dd1fdd28214263d493d859a26a6a7d3c7f
Wei Chong <weichong148@gmail.com>
2021-11-08 22:29:53 +0500

    Complete UI components.

--- src/client/js/components/pages/WalletHome.jsx
+++ src/client/js/components/pages/WalletHome.jsx
@@ -84,13 +84,120 @@ export default function WalletHome() {
             <div className="columns is-centered">
                 <div className="column card-container">
                     <div className="card wallets-page-card">
- 
                         <div className="tokens-table-title-container">
                             <span className="tokens-table-title-container__main">Assets</span>
                             <span className="tokens-table-title-container__sub">Don't see your assets?</span>
                         </div>
 
                         <AssetsTable tokenData={data} />
+                    </div>
+                </div>
+            </div>
+
+            <div className="columns is-centered">
+                <div className="column card-container">
+                    <div className="card wallets-page-card">
+                        <div className="columns portfolio-balance">
+                            <div className="column">
+                                <h6 className="portfolio-balance__main-heading">Portfolio Balance</h6>
+                            </div>
+                        </div>
+
+                        <div className="columns">
+                            <div
+                                className="column"
+                                style={{
+                                    display: "flex",
+                                    alignItems: "center",
+                                    flexDirection: "column",
+                                }}
+                            >
+                                <img style={{ marginTop: "49px", width: "62px" }} src="/images/group.svg" alt="Connect Wallet" />
+                                <h2
+                                    style={{
+                                        fontFamily: "Gilroy",
+                                        fontStyle: "normal",
+                                        fontWeight: "bold",
+                                        fontSize: "18px",
+                                        lineHeight: " 22px",
+                                        color: "#333333",
+                                    }}
+                                >
+                                    Your crypto balance will show up here.
+                                </h2>
+                                <h3
+                                    style={{
+                                        marginTop: "9px",
+                                        fontFamily: "Gilroy",
+                                        fontStyle: "normal",
+                                        fontWeight: "normal",
+                                        fontSize: "14px",
+                                        lineHeight: "16px",
+                                        textAlign: "center",
+                                        color: "#A3A5A6",
+                                    }}
+                                >
+                                    Connect your wallet to display up to date price and balances.
+                                </h3>
+
+                                <button style={{ marginTop: "30px", marginBottom: "70px" }} class="button is-success">
+                                    Connect Wallet
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="columns is-centered">
+                <div className="column card-container">
+                    <div className="card wallets-page-card">
+                        <div className="tokens-table-title-container">
+                            <span className="tokens-table-title-container__main">Assets</span>
+                        </div>
+
+                        <div className="columns">
+                            <div
+                                className="column"
+                                style={{
+                                    display: "flex",
+                                    alignItems: "center",
+                                    flexDirection: "column",
+                                }}
+                            >
+                                <img style={{ marginTop: "49px", width: "62px" }} src="/images/group.svg" alt="Connect Wallet" />
+                                <h2
+                                    style={{
+                                        fontFamily: "Gilroy",
+                                        fontStyle: "normal",
+                                        fontWeight: "bold",
+                                        fontSize: "18px",
+                                        lineHeight: " 22px",
+                                        color: "#333333",
+                                    }}
+                                >
+                                    Your crypto assets will show up here.
+                                </h2>
+                                <h3
+                                    style={{
+                                        marginTop: "9px",
+                                        fontFamily: "Gilroy",
+                                        fontStyle: "normal",
+                                        fontWeight: "normal",
+                                        fontSize: "14px",
+                                        lineHeight: "16px",
+                                        textAlign: "center",
+                                        color: "#A3A5A6",
+                                    }}
+                                >
+                                    Add assets to your wallet to display up to date prices and balances..
+                                </h3>
+
+                                <button style={{ marginTop: "30px", marginBottom: "70px" }} class="button is-success">
+                                   Start Trading
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
