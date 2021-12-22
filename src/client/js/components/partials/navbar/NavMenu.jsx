33de5816caa34ef3b4eebde5291030f78c193b50
tiger-pko <kiru@polkaswitch.com>
2021-12-22 21:41:50 +0000

    add sandbox Moonpay link

--- src/client/js/components/partials/navbar/NavMenu.jsx
+++ src/client/js/components/partials/navbar/NavMenu.jsx
@@ -31,7 +31,8 @@ export default function NavMenu(props) {
       <NavLink exact className="nav-link" activeClassName="active" to="/swap" onClick={(e) => handleClick(true)}>Trade</NavLink>
       <NavLink className="nav-link" activeClassName="active" to="/bridge" onClick={(e) =>handleClick(false)}>Bridge</NavLink>
       <NavLink exact className="nav-link" activeClassName="active" to="/wallet">Wallet</NavLink>
-      <a className="nav-link" href="https://buy-staging.moonpay.com?apiKey=pk_test_5ZOE1A7xmiXrfUrnzuVlH4DunwykjRgX" target="_blank">Buy</a>
+      <a className="nav-link"
+        href="https://buy-staging.moonpay.com?apiKey=pk_test_5ZOE1A7xmiXrfUrnzuVlH4DunwykjRgX&colorCode=%2358b57e" target="_blank">Buy</a>
     </div>
   );
 }
